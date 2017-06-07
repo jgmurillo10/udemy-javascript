2f1e25a26a2b6430a4b659fc5cb8421638b7a64d
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-06-06 22:28:49 -0500

    section2-12:ExecutionContext

---
+++ section2/B6_Execution/Starter/app.js
@@ -0,0 +1,18 @@
+/**
+  executes code that you have written line by line
+  interpreting it
+  converting it
+  compiling it
+  executing it on the computer
+**/
+'use strict';
+function b() {
+    console.log('Called b!');
+}
+
+b();
+
+console.log(a);
+
+var a = 'Hello World!';
+console.log(a);
