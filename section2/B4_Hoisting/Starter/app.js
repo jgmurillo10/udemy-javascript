f1c8c05b392238d03c2c32bb00958a91dea6b8a8
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-06-06 21:48:43 -0500

    section2-10:Hoisting

--- section2/B4_Hoisting/Starter/app.js
+++ section2/B4_Hoisting/Starter/app.js
@@ -1,5 +1,28 @@
+b();
+console.log(a);
+
 var a = 'Hello World!';
 
 function b() {
-    
+    console.log('called b!');
 }
+
+/**
+It is not like the var declarations code are moved to the top.
+like var a;
+console.log(a);
+a = 'aa';
+Execution context is created in two phases
+1. Creation:
+  Global object
+  this
+  outer environment
+    setup memory space for vars and functions
+    "hoisting"
+    (before code runs line by line, js engine has already set aside memory space for variables and functions)
+    vars and funcs exists in memory.
+2. Execution
+when the assignments are made
+kind of a=b;
+in creation phase js engine doesnot know value of a, instead asign value undefined (placeholder)
+  **/
---
+++ section2/B5_undefined/Starter/app.js
@@ -0,0 +1,12 @@
+/**
+Conceptual Aside: JS and 'undefined'
+
+**/
+b();
+console.log(a);
+
+var a = 'Hello World!';
+
+function b() {
+    console.log('Called b!');
+}
