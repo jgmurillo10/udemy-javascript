131c4f65302f2b2392c8608ea1104680891a0bfb
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-08-07 12:11:52 -0500

    Add Faking namespaces

---
+++ section4/D3_FakingNamespaces/Starter/app.js
@@ -0,0 +1,22 @@
+var greet = 'Hello!';
+var greet = 'Hola!';
+
+console.log(greet);
+
+
+let english = {
+  greetings: {
+    basic: 'Hello!',
+    normal: 'Bye'
+  },
+  languages: [
+    {
+      spanish: 'spa',
+      english: 'eng'
+    },
+    {
+      
+    }
+  ]
+};
+console.log(english);
