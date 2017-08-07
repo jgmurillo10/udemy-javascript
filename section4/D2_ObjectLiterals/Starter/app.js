401f790cf3ff6344c430f3a69be11e719ac179c1
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-08-06 19:45:41 -0500

    Section 34

---
+++ section4/D2_ObjectLiterals/Starter/app.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+  var person = {
+    firstname: 'Juan',
+    lastname: 'Murillo',
+    address: {
+      city: 'New York',
+      state: 'NY',
+      street: '111 Main St.',
+    }
+  };
+  console.log(person);
+})();
