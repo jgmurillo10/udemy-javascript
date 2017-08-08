46693848ca41ecdaaf172870a75bd34b3de3ef8a
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-08-07 19:45:56 -0500

    Function Factories

---
+++ section4/D16b_ClosuresPart2/Starter/app.js
@@ -0,0 +1,35 @@
+function buildFunction() {
+  var arr = [];
+  for (var i = 0; i < 3; i++) {
+    arr.push(function () {
+      console.log(i);
+    });
+  };
+  return arr;
+}
+
+
+var fs = buildFunction();
+fs[0]();
+fs[1]();
+fs[2]();
+
+console.log('-------');
+function buildFunction2() {
+  var arr = [];
+  for (var i = 0; i < 3; i++) {
+    (function (j) {
+      arr.push(function () {
+        console.log(j);
+      });
+    })(i);
+
+  };
+  return arr;
+}
+
+
+var fs2  = buildFunction2();
+fs2[0]();
+fs2[1]();
+fs2[2]();
